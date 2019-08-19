442c2950def2dc4850626850c6909f24b4e25ce3
eric <eric@watcha.com>
2019-08-20 01:23:35 +0900

    Conver class component to hook

--- src/client/components/HeaderAdmin.js
+++ src/client/components/HeaderAdmin.js
@@ -1,45 +1,37 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { BrowserRouter as Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class HeaderAdmin extends Component {
-  constructor(props) {
-    super(props);
-
-    this.onBackdropClick = this.onBackdropClick.bind(this);
-  }
-
-  componentDidMount() {
-    window.document.body.addEventListener('click', this.onBackdropClick);
-  }
-
-  componentWillUnmount() {
-    window.document.body.removeEventListener('click', this.onBackdropClick);
-  }
-
-  onBackdropClick(ev) {
-    const headerAdmin = ev.target.closest('.header__admin-modal');
-    const userIcon = ev.target.closest('.icon-user');
+const HeaderAdmin = (props) => {
+  const onBackdropClick = useCallback((e) => {
+    const headerAdmin = e.target.closest('.header__admin-modal');
+    const userIcon = e.target.closest('.icon-user');
 
     if (!userIcon && !headerAdmin) {
-      this.props.onBackgroundClick();
+      props.onBackgroundClick();
     }
-  }
+  });
 
-  render() {
-    return (
-      <ul className="header__admin-modal" onClick={this.props.onClick}>
-        <Link to="/">Home</Link>
-        <Link to="/user-account">My account</Link>
-        <Link to="/new-article">New article</Link>
-        <a href="/api/logout">Sign out</a>
-      </ul>
-    );
-  }
-}
+  useEffect(() => {
+    window.document.body.addEventListener('click', onBackdropClick);
+    return () => {
+      window.document.body.removeEventListener('click', onBackdropClick);
+    };
+  });
+
+  return (
+    <ul className="header__admin-modal" onClick={props.onClick}>
+      <Link to="/">Home</Link>
+      <Link to="/user-account">My account</Link>
+      <Link to="/new-article">New article</Link>
+      <a href="/api/logout">Sign out</a>
+    </ul>
+  );
+};
 
 export default HeaderAdmin;
 
 HeaderAdmin.propTypes = {
   onBackgroundClick: PropTypes.func,
+  onClick: PropTypes.func.isRequired
 };
